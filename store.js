bbb537aad90791c2a901d27a97e5d2029835a4e1
Jany Battung <janybattung@gmail.com>
2020-06-26 01:38:01 -0700

    add file

---
+++ store.js
@@ -0,0 +1,35 @@
+const STORE = {
+    questions: [//1
+      {
+        question: "What is the smallest planet in the Solar System?",
+        options: ["Venus", "Jupiter", "Mercury", "Uranus"],
+        answer: "Mercury"
+      },
+      //2
+      {
+        question: "What is the largest planet in the Solar System?",
+        options: ["Jupiter", "Mars", "Pluto", "Venus"],
+        answer: "Jupiter"
+      },
+      //3
+      {
+        question: "What is the hottest planet in the Solar System?",
+        options: ["Uranus", "Neptune", "Earth", "Venus"],
+        answer: "Venus"
+      },
+      //4
+      {
+        question: "What planet in the solar system is farthest from the Sun?", 
+        options: ["Neptune", "Earth", "Mercury", "Saturn"],
+        answer: "Neptune"
+      },
+      //5
+      {
+        question: "What is the second smallest planet in the solar system?",
+        options: ["Mercury", "Mars", "Jupiter", "Pluto"],
+        answer: "Mars"
+      },
+    ],
+    currentQuestion: 0,
+    score: 0
+  };
